eb16facb2e8f68b2fe5068476ab2455e307f17ec
Gregor Woiwode <gw@co-it.eu>
2017-05-23 09:53:50 +0200

    (fix) switch back to dist/ while ngu-app-shell is not aware of custom distribution dirs

--- pwa.sh
+++ pwa.sh
@@ -5,7 +5,7 @@ shopt -s globstar
 set -x
 
 # Production build
-./node_modules/.bin/ng build --prod --outputPath ./dist-pwa
+./node_modules/.bin/ng build --prod --outputPath ./dist
 
 # [Workaround] Copy template and styles to root folder (see: https://github.com/alxhub/io17/issues/8)
 cp ./src/**/*.{html,css,sass} .
@@ -14,17 +14,17 @@ cp ./src/**/*.{html,css,sass} .
 ./node_modules/.bin/ngu-app-shell --module src/app/app.module.ts \
                                   --url /loading \
                                   --insert src/app/loading/loading.module.ts \
-                                  --out dist-pwa/index.html
+                                  --out dist/index.html
 
 # Copy prebuilt worker in oure site
-cp node_modules/@angular/service-worker/bundles/worker-basic.min.js dist-pwa/
+cp node_modules/@angular/service-worker/bundles/worker-basic.min.js dist/
 
 # Copy service worker manifest (til now without tooling)
 ./node_modules/.bin/ngu-sw-manifest --module src/app/app.module.ts \
-                                    --out dist-pwa/ngsw-manifest.json
+                                    --out dist/ngsw-manifest.json
 
 # [Workaround] Remove template and styles to root folder (see: https://github.com/alxhub/io17/issues/8)
 rm "./"*.{html,css,sass}
 
 # Serve
-./node_modules/.bin/http-server dist-pwa/
+./node_modules/.bin/http-server dist/
