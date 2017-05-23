d92ca9fd104272f223e87d6ab6850dcc46146ee7
Gregor Woiwode <gw@co-it.eu>
2017-05-23 15:52:53 +0200

    (universal) add hints to build script

--- universal.sh
+++ universal.sh
@@ -10,3 +10,11 @@ set -x
 
 # Start server
 ./node_modules/.bin/ts-node ./src/server.ts
+
+
+# Have a look at changes in
+# - tsconfig.json
+# - tsconfig.app.json
+# - app.server.module.ts
+# - server.ts
+
